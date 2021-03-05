cd573b183122604f467cbb6755ac43811483ac15
Gabriel-Lemus <glemus.stuart@gmail.com>
2021-03-05 11:42:46 -0600

    Enhanced UX + added Firestore DB support:   - Added helpful messages for signup/login.   - Created user DB collection in Firestore.   - Newly signed up users are added automatically to user DB collection.   - Logged in users can create new items, add their names and quantity.   - A company DB collection is created when a user adds the first item to the company's inventory.

--- public/scripts/auth/auth.js
+++ public/scripts/auth/auth.js
@@ -2,7 +2,6 @@
 firebase.initializeApp({
   projectId: 'product-inventory-manager',
   appId: '1:729377056436:web:7c321d1ecdd3ba831a59e1',
-  databaseURL: 'https://product-inventory-manager-default-rtdb.firebaseio.com',
   apiKey: 'AIzaSyDi3hy7PunKTxs4vAPpmsaWMPMbnuQUhqE',
   authDomain: 'product-inventory-manager.firebaseapp.com',
 });
@@ -14,34 +13,65 @@ const db = firebase.firestore();
 // Update Firestore settings.
 db.settings({ timestampsInSnapshots: true });
 
-// End of Firebase setup
+// End of Firebase setup.
 
-// Listen for authentication status changes
+// User Object.
+let _user = null;
+
+// Listen for authentication status changes.
 auth.onAuthStateChanged((user) => {
+  _user = user;
+
   if (user) {
-    // User is logged in
-    // Get db data
-    db.collection('items')
+    // User is logged in; get db data.
+    db.collection('users')
+      .doc(user.uid)
       .get()
-      .then((snapshot) => {
-        setupItems(snapshot.docs);
+      .then((doc) => {
+        db.collection(doc.data().Company_Name + '-items').onSnapshot(
+          (snapshot) => {
+            setupItems(snapshot.docs);
+          }
+        );
       });
-
-    document.getElementById('account-details-li').style.display = '';
-    document.getElementById('logout-li').style.display = '';
-    document.getElementById('create-new-item-li').style.display = '';
-    document.getElementById('login-li').style.display = 'none';
-    document.getElementById('sign-up-li').style.display = 'none';
+    setupUI(user);
   } else {
-    // User logged out
-    setupItems([]);
-
-    document.getElementById('account-details-li').style.display = 'none';
-    document.getElementById('logout-li').style.display = 'none';
-    document.getElementById('create-new-item-li').style.display = 'none';
-    document.getElementById('login-li').style.display = '';
-    document.getElementById('sign-up-li').style.display = '';
+    // User is logged out.
+    _user = null;
+    setupUI();
+    showPageInfo();
   }
+});
+
+// Add new item to inventory.
+const newItemForm = document.querySelector('#new-item-form');
+newItemForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let loader = document.getElementById('add-new-item-loader');
+  loader.style.display = 'block';
+
+  // Attempt to add new item to inventory or catch the error.
+  db.collection('users')
+    .doc(_user.uid)
+    .get()
+    .then((doc) => {
+      db.collection(doc.data().Company_Name + '-items')
+        .doc(newItemForm['item-name'].value)
+        .set({
+          itemName: newItemForm['item-name'].value,
+          quantity: parseInt(newItemForm['item-quantity'].value),
+        })
+        .then(() => {
+          // Reset form and close the modal.
+          const modal = document.querySelector('#modal-create-new-item');
+          M.Modal.getInstance(modal).close();
+          newItemForm.reset();
+          loader.style.display = 'none';
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    });
 });
 
 // Sign Up
@@ -52,36 +82,111 @@ signUpForm.addEventListener('submit', (e) => {
   // Getting user info
   const email = signUpForm['signup-email'].value;
   const password = signUpForm['signup-password'].value;
+  const passwordConfirmation = signUpForm['signup-password-confirmation'].value;
+  let loader = document.getElementById('sign-up-loader');
+  loader.style.display = 'block';
 
-  // Sign Up user
-  auth.createUserWithEmailAndPassword(email, password).then((credential) => {
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signUpForm.reset();
-  });
+  setTimeout(() => {
+    // Check if passwords match
+    if (password === passwordConfirmation) {
+      // Sign up user
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((cred) => {
+          return db.collection('users').doc(cred.user.uid).set({
+            Name: signUpForm['user-name'].value,
+            Company_Name: signUpForm['company-name'].value,
+            Email: signUpForm['signup-email'].value,
+          });
+        })
+        .then(() => {
+          const modal = document.querySelector('#modal-signup');
+          M.Modal.getInstance(modal).close();
+          signUpForm.reset();
+          loader.style.display = 'none';
+          location.reload();
+        });
+    } else {
+      // Passwords do not match.
+      M.toast({ html: 'Passwords do not match.' });
+      let signUp = document.getElementById('signup-password-confirmation');
+      signUp.value = '';
+      signUp.classList.remove('active');
+      signUp.classList.remove('valid');
+      loader.style.display = 'none';
+    }
+  }, 1750);
 });
 
 // Logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut();
+  let loader = document.getElementById('logout-loader');
+  loader.style.display = 'block';
+  setTimeout(() => {
+    auth.signOut();
+    loader.style.display = 'none';
+    location.reload();
+  }, 1750);
 });
 
 // Login
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  let loader = document.getElementById('login-loader');
+  loader.style.display = 'block';
 
   // Get user info
   const email = loginForm['login-email'].value;
   const password = loginForm['login-password'].value;
 
   // Log user in
-  auth.signInWithEmailAndPassword(email, password).then(() => {
-    // Close signup modal & reset form
-    const modal = document.querySelector('#modal-login');
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
-  });
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      // Close signup modal & reset form
+      const modal = document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+      loader.style.display = 'none';
+      location.reload();
+    })
+    .catch((error) => {
+      setTimeout(() => {
+        let loginPasswordInput = document.getElementById('login-password');
+        let loader = document.getElementById('login-loader');
+        if (
+          error.message ==
+          'The password is invalid or the user does not have a password.'
+        ) {
+          M.toast({
+            html: 'Your password is incorrect. Please re-enter your password.',
+          });
+          loginPasswordInput.value = '';
+          loginPasswordInput.classList.remove('active');
+          loginPasswordInput.classList.remove('valid');
+          console.clear();
+        } else if (
+          error.message ==
+          'There is no user record corresponding to this identifier. The user may have been deleted.'
+        ) {
+          M.toast({
+            html:
+              "We couldn't find a user associated with this email address. Please make sure your credentials are correct.",
+          });
+          loginPasswordInput.value = '';
+          loginPasswordInput.classList.remove('active');
+          loginPasswordInput.classList.remove('valid');
+          console.clear();
+        } else {
+          M.toast({
+            html: 'Something went wrong. Please re-enter your information.',
+          });
+          console.log(error.message);
+        }
+        loader.style.display = 'none';
+      }, 750);
+    });
 });
--- public/scripts/auth/index.js
+++ public/scripts/auth/index.js
@@ -1,5 +1,36 @@
 // DOM elements
 const itemList = document.querySelector('.items');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
+
+// Setup UI based on user logged status.
+const setupUI = (user) => {
+  if (user) {
+    // Show account info.
+    db.collection('users')
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        const accountDetHtml = `
+        <h6>Hello, ${doc.data().Name}!</h6>
+        <h6>Logged in as ${user.email}</h6>
+        <h6>Company: ${doc.data().Company_Name}</h6>`;
+
+        accountDetails.innerHTML = accountDetHtml;
+      });
+
+    // Toggle UI elements.
+    loggedInLinks.forEach((link) => (link.style.display = 'block'));
+    loggedOutLinks.forEach((link) => (link.style.display = 'none'));
+  } else {
+    // Hide account info.
+    accountDetails.innerHTML = '';
+
+    loggedInLinks.forEach((link) => (link.style.display = 'none'));
+    loggedOutLinks.forEach((link) => (link.style.display = 'block'));
+  }
+};
 
 // Setup Items
 function setupItems(data) {
@@ -18,11 +49,60 @@ function setupItems(data) {
     itemList.innerHTML = html;
   } else {
     itemList.innerHTML = `
-    <h5>About this project:</h5>
-    <p>This is webapp is aimed to allow users to manage thier inventories and create alerts whenever they will perish or be depleted.</p>
-    <br>
-    <h6 style="text-align: center">You are currently logged out. Please login to see your companies\' items</h6>`;
+      <p>Your company currently doesn't have any available items.</p>
+      <p>Press the 'Add Item' button on the top right corner to add a new item to your company's inventory.</p>`;
   }
+}
+
+// Show page info to users that are not signed in.
+function showPageInfo() {
+  itemList.innerHTML = `
+    <h5>About this project</h5>
+    <p class="flow-text">Numerous restaurants, groceries, retailers and individuals may face issues related to managing their product inventories, especially when it comes to food. Inventory mismanagement is rampant across the industry despite the best efforts of operations managers, employees, and companies. Mismanagement may result in canceled orders due to inventory shortages, which in turn, lead to revenue losses.</p>
+    <p class="flow-text">This project is aimed to solve this problem. This product inventory manager is a webapp that will allow users to keep a tab on their inventories or other items as well as to create alerts whenever perishables will spoil or will deplete.</p>
+    <br>
+      <div><img id="inv-mngmnt-png" src="./img/inventory_management.png" /></div>
+    <p id="img-quote" class="center-align">This web app is aimed to allow users to easily manage their inventories through the use of an intuitive interface.</p>
+    <br>
+    <h5>Key characteristics</h5>
+    <div class="key-char-div">
+      <ul class="list">
+        <li><em><b><p class="flow-text">Inventory items upload and update</p></b><em></li>
+        <div class="key-char-div">
+          <ul class="list nested-list">
+            <li class="inner-li-txt"><p class="flow-text">The user will be able to upload items and update their quantities by entering data like the product name, quantity, vendor, and Universal Product Code (UPC).</p></li>
+            <li class="inner-li-txt"><p class="flow-text">This will allow to eliminate material waste by cycling through the user's inventory in a periodic basis.</p></li>
+            <li class="inner-li-txt"><p class="flow-text">It will also enable the user to know their stock level in real time.</p></li>
+            <li class="inner-li-txt"><p class="flow-text">Better control of item costs.</p></li>
+            <li class="inner-li-txt"><p class="flow-text">This will help users minimize human error as well.</p></li>
+          </ul>
+        </div>
+        <li><em><b><p class="flow-text">Low-stock inventory alerts</p></b><em></li>
+        <div class="key-char-div">
+          <ul class="list nested-list">
+            <li class="inner-li-txt"><p class="flow-text">The webapp will notify the user whenever a product is about to spoil or run out.</p></li>
+          </ul>
+        </div>
+        <li><em><b><p class="flow-text">Reports</p></b><em></li>
+        <div class="key-char-div">
+          <ul class="list nested-list">
+            <li class="inner-li-txt"><p class="flow-text">Analytics and reports will provide the user with a thorough picture of their finances, performance, and periodic activities.</p></li>
+            <li class="inner-li-txt"><p class="flow-text">The analysis of this information will enable the user to increase their efficiency as well as profitability.</p></li>
+            <li class="inner-li-txt"><p class="flow-text">The reports will include stock levels, profitability, and sales performance (the latter two will only be available to groceries or restaurants).</p></li>
+            <div class="key-char-div">
+              <ul class="list nested-list">
+                <li class="inner-li-txt"><p class="flow-text"><b>Stock reports</b> let the user stay updated on what items they currently have available.</p></li>
+                <li class="inner-li-txt"><p class="flow-text"><b>Profitability reports</b> can help the user visualize their income flow by comparing outflows with inflows. Also, they will be able to know their profit margins for items.</p></li>
+                <li class="inner-li-txt"><p class="flow-text"><b>Sales reports</b> will let the user see which items are not selling as well as the others so they could consider removing them. Additionally, they can quickly tell which items are the least ordered and most ordered to make sure they have it on stock.</p></li>
+              </ul>
+            </div>
+          </ul>
+        </div>
+      </ul>
+    </div>
+    <br>
+    <h6 class="center-align">You are currently logged out.</h6>
+    <p class="center-align">Please login to check your company's items.</p>`;
 }
 
 // Setup materialize components
